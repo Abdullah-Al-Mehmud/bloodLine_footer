6d39a5f19a3198190c6fa76f859a3e6100505d4e
mehmud <timehmud121@gmail.com>
2023-05-21 21:03:05 +0600

    new update

---
+++ main.js
@@ -0,0 +1,4 @@
+const year = new Date().getFullYear();
+const date = `&copy; copyrights  ${year} | All Rights reserved by <span>Blood Line</span>`;
+
+let cpy_right = (document.getElementById("cpy_right").innerHTML = date);
